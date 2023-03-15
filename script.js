d60aa146d9283164e9dfdb756f7a3a59c0f2edfa
Rey  Baterbonia <poochinng@gmail.com>
2023-03-15 23:39:29 +0800

    init

---
+++ script.js
@@ -0,0 +1,6 @@
+const menu = document.getElementsByClassName('menu')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+menu.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+})
